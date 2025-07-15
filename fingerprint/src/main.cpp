fdd27f1fb09695c308904b9682480204e1568ffb
daliso banda <bandadaliso255@gmail.com>
2025-07-15 20:37:07 +0200

    finger detection functionality; implement setup and detection methods for triggering GPIO pin based on fingerprint sensor input. the pin is pin 13 ..will be connected to the camera and the camera will take a picture based off this

--- fingerprint/src/main.cpp
+++ fingerprint/src/main.cpp
@@ -2,26 +2,32 @@
 #include "sensor/FingerprintSensor.h"
 #include "enroll/Enroll.h"
 #include "search/Search.h"
+#include "search/detectFinger.h"
+#define CAMERA_PIN 13 //pin for camera trigger
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("ESP32 Fingerprint System");
+  pinMode(CAMERA_PIN, OUTPUT);
+  digitalWrite(CAMERA_PIN, LOW);
   setupFingerprintSensor();
+   setupFingerDetection();   
 }
 
 void loop() {
-  Serial.println("\nChoose Option:\n1 - Enroll\n2 - Search");
-  while (!Serial.available());
-  char option = Serial.read();
-  while (Serial.available()) Serial.read();
+  // Serial.println("\nChoose Option:\n1 - Enroll\n2 - Search");
+  // while (!Serial.available());
+  // char option = Serial.read();
+  // while (Serial.available()) Serial.read();
 
-  if (option == '1') {
-    uint8_t id = readNumber();
-    enrollFingerprint(id);
-  } else if (option == '2') {
-    searchFingerprint();
-  }
+  // if (option == '1') {
+    // uint8_t id = readNumber();
+    // enrollFingerprint(id);
+  // } else if (option == '2') {
+    // searchFingerprint();
+  // }
 
+detectFingerAndTrigger();
   delay(2000);
 }
